fb05e0541532c8fb21f74b5506498a38f73bf4f1
Maxwell Stone <38924020+maxhs2014@users.noreply.github.com>
2021-11-25 22:58:40 -0500

    latest v1

--- app.config.js
+++ app.config.js
@@ -33,7 +33,7 @@ export default {
     },
     ios: {
       supportsTablet: false,
-      buildNumber: "20",
+      buildNumber: "21",
       bundleIdentifier: "com.partynearyou.app",
       config: {
         googleMobileAdsAppId: "ca-app-pub-5790083206239403~2410650773" // sample id, replace with your own
